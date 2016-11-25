baa4e3fb9f80e36221fcd0b42b9c9498d481b264
santiagoMC92 <santiago_mc_92@hotmail.com>
2016-11-25 14:54:54 -0600

    primer pull

--- core/core.php
+++ core/core.php
@@ -4,9 +4,9 @@ session_start();
 
 #Constantes de conexión
 define('DB_HOST','127.0.0.1');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','apweb');
+define('DB_USER','u326008933_apweb');
+define('DB_PASS','Cr3d1t0Gl0bal');
+define('DB_NAME','u326008933_apweb');
 define('HTML_DIR','pages/');
 define('PATH_COMPLEMENTO','');
 
